50e0bfe296ee6aa358feb6ed30eb83358e650175
Enda McCormack <endamccormack0@gmail.com>
2015-03-28 03:27:21 +0000

    - Added the structure of the initial 3 jobs that its going to create in Jenkins - Added a basic job for migrating my db scripts project

--- src/main/groovy/ContextualDatabaseJob.groovy
+++ src/main/groovy/ContextualDatabaseJob.groovy
@@ -8,23 +8,23 @@ service.build(this);
 class ContextualDatabase{
   def project
   def repoName
-  def github = 'git@github.com'
+  def github = 'https://github.com'
 
   def build(dslFactory) {
-    def serviceCommand = "/etc/init.d/dwp-${serviceName} restart"
+    def serviceCommand = "/usr/bin/mysql -u root -p1Password2 < dumpfilename.sql"
 
     dslFactory.job {
       name "${project}"
       description 'This Job is automatically generated using JobDSL that migrates the old db with a script, any manual changes will be overridden.'
 
       scm {
-          git "${github}:${repoName}.git", "*/master"
+          git "${github}/${repoName}.git", "*/master"
       }
       triggers {
         scm "* * * * *"
       }
       steps {
-        shell("/usr/bin/mysql -u root -p1Password2 $WORKSPACE/dumpfilename.sql")
+        shell(serviceCommand)
       }
     }
   }
