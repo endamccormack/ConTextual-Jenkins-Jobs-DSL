3dc8975cedcd8c542a5e1afb9a2d564b8ca5e21d
Enda McCormack <endamccormack0@gmail.com>
2015-03-28 04:33:01 +0000

    Added in the configuration for the API job

---
+++ src/main/groovy/ContextualAPIJob.groovy
@@ -0,0 +1,35 @@
+def service = new ContextualAPI(
+  project:"ContextualAPI",
+  repoName:"endamccormack/ConTextualAPI",
+)
+
+service.build(this);
+
+class ContextualAPI{
+  def project
+  def repoName
+  def github = 'https://github.com'
+
+  def build(dslFactory) {
+    def serviceCommandRemoveIvyLock = "rm -f /opt/play-2.2.2/framework/sbt/boot/sbt.boot.lock /opt/play-2.2.2/repository/.sbt.ivy.lock"
+    def serviceCommandCompile = "/usr/bin/play clean compile stage"
+    def serviceCommandRun = "BUILD_ID=dontKillMe target/universal/stage/bin/contextualapi &"
+
+    dslFactory.job {
+      name "${project}"
+      description 'This Job is automatically generated using JobDSL that starts the API, any manual changes will be overridden.'
+
+      scm {
+          git "${github}/${repoName}.git", "*/master"
+      }
+      triggers {
+        scm "* * * * *"
+      }
+      steps {
+        shell(serviceCommandRemoveIvyLock)
+        shell(serviceCommandCompile)
+        shell(serviceCommandRun)
+      }
+    }
+  }
+}
